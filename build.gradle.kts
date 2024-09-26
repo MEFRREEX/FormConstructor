f190af4d2ce67deeba0ec8089810ac7032d5dd0e
MEFRREEX <mefrreex@gmail.com>
2024-09-26 11:20:51 +0300

    feat: migration to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("java")
+}
+
+group = "com.mefrreex.formconstructor"
+description = "formconstructor"
+version = "2.0.3"
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+    maven("https://repo.opencollab.dev/maven-releases/")
+    maven("https://repo.opencollab.dev/maven-snapshots/")
+}
+
+dependencies {
+    compileOnly("com.github.PowerNukkitX:PowerNukkitX:master-SNAPSHOT")
+    compileOnly("com.google.code.gson:gson:2.11.0")
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<ProcessResources> {
+    filesMatching("*.yml") {
+        expand(project.properties)
+    }
+}
