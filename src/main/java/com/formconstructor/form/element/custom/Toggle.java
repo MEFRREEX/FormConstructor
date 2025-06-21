5b5b16cb9eccb2b24dc309e4c6730fe41c2a28c7
Arkadiy <mefrreex@gmail.com>
2025-06-21 10:57:34 +0400

    feat: added api for tooltips for custom elements

--- src/main/java/com/formconstructor/form/element/custom/Toggle.java
+++ src/main/java/com/formconstructor/form/element/custom/Toggle.java
@@ -2,15 +2,18 @@ package com.formconstructor.form.element.custom;
 
 import com.formconstructor.form.element.ElementCustom;
 import com.formconstructor.form.element.ElementIdentifiable;
+import com.formconstructor.form.element.ElementTooltip;
 import com.formconstructor.form.element.ElementType;
 import com.google.gson.annotations.SerializedName;
 
-public class Toggle extends ElementIdentifiable implements ElementCustom {
+public class Toggle extends ElementIdentifiable implements ElementCustom, ElementTooltip {
 
     @SerializedName("default") 
     private final boolean defaultValue;
 
     private transient boolean value;
+
+    private String tooltip = "";
 
     public Toggle() {
         this("");
@@ -36,6 +39,17 @@ public class Toggle extends ElementIdentifiable implements ElementCustom {
 
     public boolean getValue() {
         return value;
+    }
+
+    @Override
+    public String getTooltip() {
+        return tooltip;
+    }
+
+    @Override
+    public Toggle setTooltip(String tooltip) {
+        this.tooltip = tooltip;
+        return this;
     }
 
     @Override
